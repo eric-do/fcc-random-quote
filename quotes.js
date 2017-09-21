ca4e7700a620bc8d9595d65bc92118ba9b37d7cf
Eric Do <ericdo.617@gmail.com>
2017-09-21 12:28:24 -0700

    Created html, css, js files. Set up non-API functionality.

---
+++ quotes.js
@@ -0,0 +1,8 @@
+console.log("CONNECTED");
+
+$(document).ready(function(){
+    // Pull random quote here
+    $("#btn-generate").on("click", function() {
+        $(".quote-display").html("Hello");
+    });
+});
