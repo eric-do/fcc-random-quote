8a4c3d31385cde00f225dee8b0b5ad6e6372e798
Eric Do <ericdo.617@gmail.com>
2017-09-22 00:11:23 -0700

    Updated quotes to all by key rather than iteration.

--- quotes.js
+++ quotes.js
@@ -2,22 +2,22 @@ console.log("CONNECTED");
 
 $(document).ready(function(){
         // Pull random quote here
-    $("#btn-generate").on("click", function() {
+    $("#new-quote").on("click", function() {
         //$.getJSON("https://crossorigin.me/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json){
         $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/", function(json){
-            var html = "";
+            var quote = "";
+            var author = "";
             var index = Math.floor(Math.random() * json.length);
             var keys = Object.keys(json[index]);
             
-           
+            $("#text").html(json[index].quote); 
+            $("#author").html(json[index].author);
+
+            /*
             keys.forEach(function(key) {
-                console.log(key, json[index][key]); 
-                html += "<strong>" + key + ": </strong>" + "<em>" + json[index][key] + "</em><br>";
-            });
-            
-            $(".quote-display").html(html); 
-           
+                console.log(key, json[index][key]);                
+                quote += "<strong>" + key + ": </strong>" + "<em>" + json[index][key] + "</em><br>";
+            });*/
         });
     });
 });
-
