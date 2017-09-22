e32f610467ec503c5110af8d713783542ca56413
Eric Do <ericdo.617@gmail.com>
2017-09-22 07:54:11 -0700

    Added tweet functionality

--- quotes.js
+++ quotes.js
@@ -5,14 +5,14 @@ $(document).ready(function(){
     $("#new-quote").on("click", function() {
         //$.getJSON("https://crossorigin.me/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json){
         $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/", function(json){
-            var quote = "";
-            var author = "";
             var index = Math.floor(Math.random() * json.length);
             var keys = Object.keys(json[index]);
+            var quote = json[index].quote;
+            var author = json[index].author;
             
-            $("#text").html(json[index].quote); 
-            $("#author").html(json[index].author);
-
+            $("#text").html(quote); 
+            $("#author").html(author);
+            $("#tweet-quote").attr("href", 'https://twitter.com/intent/tweet?text="' + quote + '"%20' + author);
             /*
             keys.forEach(function(key) {
                 console.log(key, json[index][key]);                
