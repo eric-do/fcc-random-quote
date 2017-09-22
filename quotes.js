0e7bd68437e47d7c3d2152b768b30f5b97f119e6
Eric <ericdo.617@gmail.com>
2017-09-22 00:41:39 +0000

    Connected code to API

--- quotes.js
+++ quotes.js
@@ -1,8 +1,23 @@
 console.log("CONNECTED");
 
 $(document).ready(function(){
-    // Pull random quote here
+        // Pull random quote here
     $("#btn-generate").on("click", function() {
-        $(".quote-display").html("Hello");
+        //$.getJSON("https://crossorigin.me/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json){
+        $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/", function(json){
+            var html = "";
+            var index = Math.floor(Math.random() * json.length);
+            var keys = Object.keys(json[index]);
+            
+           
+            keys.forEach(function(key) {
+                console.log(key, json[index][key]); 
+                html += "<strong>" + key + ": </strong>" + "<em>" + json[index][key] + "</em><br>";
+            });
+            
+            $(".quote-display").html(html); 
+           
+        });
     });
 });
+
