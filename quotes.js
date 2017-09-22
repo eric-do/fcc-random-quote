1ce8670a956ca640918f66c5366a97300cc2816d
Eric Do <ericdo.617@gmail.com>
2017-09-22 15:49:46 -0700

    Replace placeholders with data on page load, update visual styling

--- quotes.js
+++ quotes.js
@@ -1,9 +1,15 @@
 console.log("CONNECTED");
 
+setQuote();
+
 $(document).ready(function(){
         // Pull random quote here
     $("#new-quote").on("click", function() {
-        //$.getJSON("https://crossorigin.me/https://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en", function(json){
+            setQuote();
+        });
+    });
+
+function setQuote() {
         $.getJSON("https://random-quote-generator.herokuapp.com/api/quotes/", function(json){
             var index = Math.floor(Math.random() * json.length);
             var keys = Object.keys(json[index]);
@@ -13,11 +19,5 @@ $(document).ready(function(){
             $("#text").html(quote); 
             $("#author").html(author);
             $("#tweet-quote").attr("href", 'https://twitter.com/intent/tweet?text="' + quote + '"%20' + author);
-            /*
-            keys.forEach(function(key) {
-                console.log(key, json[index][key]);                
-                quote += "<strong>" + key + ": </strong>" + "<em>" + json[index][key] + "</em><br>";
-            });*/
         });
-    });
-});
+}
